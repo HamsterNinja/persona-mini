6796feb59a2d1f960e3f18667e7fcda3e253a6f7
HamsterNinja <oohamsterninjaoo@gmail.com>
2019-12-02 15:10:31 +0300

    Разметка шапки, футера, главной

---
+++ assets/js/src/app.js
@@ -0,0 +1,16 @@
+$('.main-slick').slick({
+    slidesToShow: 1,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: false,
+    swipeToSlide: true,
+});
+$('.products-slick').slick({
+    slidesToShow: 4,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    arrows: true,
+    dots: false,
+    swipeToSlide: true,
+});
